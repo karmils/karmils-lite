ee6cc4a737178d0a2f7de4a301aab99aaa69a139
karmils <f.kakabadze@gmail.com>
2026-02-17 03:30:48 +0400

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,12 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/"
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api", "/private"]
+      }
+    ],
     sitemap: "https://www.karmils.com/sitemap.xml"
   };
 }
